755cff4c833c2ff373ad70f066ced34e41f153ff
Ta-SeenJunaid <taseen.junaid@gmail.com>
2026-01-31 04:27:07 +0100

    C++ with cmake

---
+++ src/logger/Logger.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+
+class Logger
+{
+public:
+    enum class Level
+    {
+        Info,
+        Warning,
+        Error
+    };
+
+    static void log(const std::string &message, Level level = Level::Info);
+};
